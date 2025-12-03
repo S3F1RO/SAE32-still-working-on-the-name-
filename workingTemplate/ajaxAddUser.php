b78680fb03be254ece72fe35296c63cda73db697
The User <user@email.com>
2025-12-03 17:30:22 -0300

    Lucas : Frensh -> English comments

--- workingTemplate/ajaxAddUser.php
+++ workingTemplate/ajaxAddUser.php
@@ -1,34 +1,36 @@
 <?php
-include_once('./utils.php');
-include_once('./params.php');
 
-// Vérifier si on a reçu des données
-if (!isset($_POST['data'])) {
+  include_once('./utils.php');
+  include_once('./params.php');
+
+  // Check
+  if (!isset($_POST['data'])) {
     echo json_encode([
-        "success" => false,
-        "message" => "Aucune donnée reçue"
+      "success" => false,
+      "message" => "Aucune donnée reçue"
     ]);
     exit();
-}
+  }
 
-// Décoder les données JSON envoyées
-$data = json_decode($_POST['data'], true);
+  // Decode JSON
+  $data = json_decode($_POST['data'], true);
 
-// Initialiser les variables
-$firstName = NULL;
-$lastName  = NULL;
+  // Variables initialisation
+  $firstName = NULL;
+  $lastName  = NULL;
 
-if (isset($data['firstName'])) {
+  if (isset($data['firstName'])) {
     $firstName = $data['firstName'];
-}
-if (isset($data['lastName'])) {
+  }
+  if (isset($data['lastName'])) {
     $lastName = $data['lastName'];
-}
-if (isset($data['nickname'])) {
+  }
+  if (isset($data['nickname'])) {
     $nickname = $data['nickname'];
-}
-// Réponse AJAX envoyée au JavaScript
-$data = sendAjax(URL, ["firstName" => $firstName, "lastName"  => $lastName, "nickname" => $nickname]);
-echo json_encode(["success" => true, "id" => $data["id"]]);
+  }
+
+  // Réponse AJAX envoyée au JavaScript
+  $data = sendAjax(URL, ["firstName" => $firstName, "lastName"  => $lastName, "nickname" => $nickname]);
+  echo json_encode(["success" => true, "id" => $data["id"]]);
 
 ?>
