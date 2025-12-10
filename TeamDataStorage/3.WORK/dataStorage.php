a52a0f7a9fcb254233441e2372c1da145f61c67b
The User <user@email.com>
2025-12-10 18:03:19 -0300

    Team  DataStorage: Added getVerified functions

--- TeamDataStorage/3.WORK/dataStorage.php
+++ TeamDataStorage/3.WORK/dataStorage.php
@@ -368,7 +368,6 @@ class DataStorage {
 // $idSkill = DataStorage::addSkill(36, "macabou", "le poto", "RT2", 1, "", "ffffff");
 // $idCompetence = DataStorage::addCompetence(22, 36, 21, "", 1);
 
-print_r(DataStorage::getSkillCompetences(20));
 // print_r(DataStorage::getSkill($idSkill));
 // $competencesList = 28;
 ?>
