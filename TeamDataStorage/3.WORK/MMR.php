a8a8d2fad850d70ed8851173f66610d3b7bcf10f
Sulyvan <user@email.com>
2026-01-11 08:56:47 -0300

    Sulyvan : MMR Functionnal

--- TeamDataStorage/3.WORK/MMR.php
+++ TeamDataStorage/3.WORK/MMR.php
@@ -6,7 +6,7 @@ class MMR {
 
     function __construct(){
         $this->nodes = [];
-        $this->nodes[] = new Node(0,"xxcp",0,NULL,NULL,true);
+        // $this->nodes[] = new Node(0,"xxcp",0,NULL,NULL,true);
     }
     function getNode($idNode){
         $node = $this->nodes[$idNode];
@@ -28,51 +28,64 @@ class MMR {
         $this->recursivelyAddParentFromNode($leaf);
     }
 
-    function getPeakOfLevel($level){
+    function getPeakOfLevel($level,$idNode){
         foreach ($this->nodes as $node){
-            //Get level of each node
-            if ($this->getNodeLevel($node->level) == $level){
-                //Check if peak
-                if ($node->isPeak) return $node;
-                else return NULL;
-            };
+            //Get level of each node & Check if is Peak
+            if ($this->getNodeLevel($node->idNode) == $level && $node->isPeak && $node->idNode !== $idNode) return $node;
         }
+        return NULL;
+    }
+
+    function getRootHash(){
+        return $this->getNodehash($this->getLastNodeId());
     }
 
     function getLastNodeId(){
-        return count($this->nodes) -1;
+        return count($this->nodes)-1;
     }
 
     function getNextLeafId($leafId){
         while ($this->getLastNodeId() <= $leafId) {
-            $this->addLeaf($this->getLastNodeId(),"",NULL,NULL,true);
+            $this->addLeaf();
         }
-        return getLastNodeId()+1;
+        return $this->getLastNodeId()+1;
     }
 
     function addNode($hash,$level,$idChild1=NULL,$idChild2=NULL,$isPeak=false){
         $newId = count($this->nodes);
-        $this->nodes[] = new Node($newId,"$hash",$level,$idChild1,$idChild2,$isPeak);
-        return $newId;                                                                 // SULYYYYY WHO IS THIS F*CKING $lastId
+        $this->nodes[] = new Node($newId,$hash,$level,$idChild1,$idChild2,$isPeak);
+        return $newId;                                                             
     }
 
     function recursivelyAddParentFromNode($idNode){
-        // $peak=$this->getPeakOfLevel()->id;
-        // $this->addParent($peak,$idNode);
-        // if ($peak) return recursivelyAddParentFromNode($peak);
-        // else return NULL;
-        $this->nodes[$idNode]->isPeak = true;
+        // Get Peak from Level
+        $level = $this->getNodeLevel($idNode);
+        $peak = $this->getPeakOfLevel($level,$idNode);
+
+        if ($peak == NULL) {
+            $this->nodes[$idNode]->isPeak = true;
+            return;
+        }
+        // If there's no peak then there's no peak to add so node is peak else add anoter parent
+        else{
+            $parentID = $this->addParent($peak->idNode,$idNode);
+            return $this->recursivelyAddParentFromNode($parentID);
+        }
     }
 
     function addParent($idChild1=NULL,$idChild2=NULL){
-        //Get parent hash based 
-        $level+=1;                                                          // $level ??? idk what is this
-        $parentNodeHash = hash("sha256",$this->nodes[$idChild1]->hash.$this->nodes[$idChild2]->hash);
+        // Get parent hash based 
+        if ($this->nodes[$idChild1] == NULL || $this->nodes[$idChild2] == NULL) return NULL;
+        // Add the parent to the newt level
+        $level = $this->nodes[$idChild1]->level+1;
+        // Get hash from Children                                                       
+        $parentNodeHash = hash("sha256",$this->getNodehash($idChild1).$this->getNodehash($idChild2));
+        // Set the children to leaf since they're not the peak anymore
         $this->nodes[$idChild1]->isPeak = false;
         $this->nodes[$idChild2]->isPeak = false;
 
-        $this->addNode($parentNodeHash,$level,$idChild1,$idChild2);
-        echo $parentNodeHash;
+        $parentId = $this->addNode($parentNodeHash,$level,$idChild1,$idChild2,true);
+        return $parentId;
 
     }
 
@@ -85,7 +98,13 @@ $MMR->addLeaf("tx1");
 $MMR->addLeaf("tx2");
 $MMR->addLeaf("tx3");
 $MMR->addLeaf("tx4");
-
+$MMR->addLeaf("tx5");
+$MMR->addLeaf("tx6");
+$MMR->addLeaf("tx7");
+$MMR->addLeaf("tx8");
+// $MMR->addLeaf("tx9");
+// echo json_encode($MMR);
+echo json_encode($MMR,JSON_PRETTY_PRINT);
 
 // ----------------------------------
 // echo "Root: " . $MMR->getRoot() . "\n";
@@ -93,7 +112,7 @@ $MMR->addLeaf("tx4");
 // $testNode = new Node(1,"xxc",1,NULL,NULL,true);
 // $testNode2 = new Node(1,"xxc",1,1,2,true);
 // $testPeak = new Node(2,"xxc",2,0,1,true);
-
+echo "Root Hash : ",$MMR->getRootHash();
 // $MMR = new MMR();
 // $MMR->addLeaf("xxx");
 // ----------------------------------
